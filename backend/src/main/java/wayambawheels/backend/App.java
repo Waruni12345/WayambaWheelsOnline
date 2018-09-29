ceaf63818e98f779ab51f7919c0b9b25bca237ed
Madushani Nawarathne <nawarathnem@gmail.com>
2018-09-29 13:19:33 +0530

    My project setupcompleted successfully

---
+++ backend/src/main/java/wayambawheels/backend/App.java
@@ -0,0 +1,13 @@
+package wayambawheels.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
